92d4686c7c42838987f5f08e0270eb906edb5f10
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2016-12-07 10:04:19 -0800

    Deleting Rows

--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -73,22 +73,24 @@ class ContactsTableViewController: UITableViewController {
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
         // Return false if you do not want the specified item to be editable.
         return true
     }
     */
 
-    /*
-    // Override to support editing the table view.
+ 
+    //Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            self.contacts.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+ 
 
     /*
     // Override to support rearranging the table view.
@@ -124,3 +126,4 @@ class ContactsTableViewController: UITableViewController {
     
 
 }
+
